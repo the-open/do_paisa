a572a557e0af9416eb8258da5f9088f8fa31d995
Hiemanshu Sharma <hiemanshu@gmail.com>
2017-08-29 16:10:35 +0530

    Generate and store URL params for IncomingWebhooks

--- app/models/incoming_webhook.rb
+++ app/models/incoming_webhook.rb
@@ -1,3 +1,11 @@
 class IncomingWebhook < Webhook
+  include Rails.application.routes.url_helpers
   belongs_to :processor, optional: false
+
+  after_create :generate_url
+
+  def generate_url
+    self.url = webhook_path(self)
+    save!
+  end
 end
