028446ea79e7681118fc573ae7ab2b1267940afa
Jonathan Ackerley <ackerley.jonathan@gmail.com>
2018-02-15 11:59:37 -0500

    Recurring webhook WIP

--- app/lib/webhook_payload.rb
+++ app/lib/webhook_payload.rb
@@ -3,6 +3,8 @@ module WebhookPayload
     case outgoing_webhook.system
     when 'identity'
       identity_webhook_payload(transaction)
+    when 'recurring'
+      recurring_webhook_payload(transaction)
     else
       default_webhook_payload(transaction)
     end
@@ -43,6 +45,32 @@ module WebhookPayload
       source: "#{transaction.source_external_id}|#{transaction.source_system}",
       source_system: transaction.source_system,
       source_external_id: transaction.source_external_id
+    }
+  end
+  
+  def recurring_webhook_payload(transaction)
+    {
+      cons_hash: {
+        firstname: transaction.donor.metadata['name'].split(' ').first,
+        lastname: transaction.donor.metadata['name'].split(' ')[1..-1].join(' '),
+        emails: [
+          { email: transaction.donor.metadata['email'] }
+        ],
+        addresses: [
+          {
+            line1: transaction.donor.metadata['address_line1'],
+            line2: transaction.donor.metadata['address_line2'],
+            town: transaction.donor.metadata['address_city'],
+            state: transaction.donor.metadata['address_state'],
+            postcode: transaction.donor.metadata['address_zip'],
+            country: transaction.donor.metadata['address_country']
+          }
+        ]
+      },
+      system: 'do_paisa',
+      external_id: transaction.id,
+      started_at: transaction.created_at,
+      current_amount: (transaction.amount / 100.to_f).round(2)
     }
   end
 end
