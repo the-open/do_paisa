fa8912c484575a900fab8865d950653567d077b7
Jonathan Ackerley <ackerley.jonathan@gmail.com>
2018-04-17 15:07:01 -0400

    Edit recurring donations

--- app/admin/recurring_donor.rb
+++ app/admin/recurring_donor.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register RecurringDonor do
-  actions :index, :show
+  actions :all, :except => [:destroy]
+  permit_params :next_charge_at, :ended_at
+  
   preserve_default_filters!
   remove_filter :donor
   filter :transactions, label: 'Transaction ID', collection: -> {
@@ -7,4 +9,21 @@ ActiveAdmin.register RecurringDonor do
   }
   filter :donor_email, as: :string
   filter :donor_name, as: :string
+
+  form do |f|
+    f.inputs do
+      f.input :amount
+      f.input :ended_at, as: :datepicker,
+                        datepicker_options: {
+                          min_date: "Date.today.to_s",
+                          max_date: "+3Y"
+                        }
+      f.input :next_charge_at, as: :datepicker,
+                        datepicker_options: {
+                          min_date: :next_charge_at,
+                          max_date: "+1Y"
+                        }
+    end
+    f.actions
+  end
 end
