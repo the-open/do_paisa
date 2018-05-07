3eff2394afe388eb41c8e9a5fdd45c76797c682c
James Rees <jarees@gmail.com>
2018-05-07 16:01:20 +0100

    Add recurring donations webhooks

--- app/models/outgoing_webhook.rb
+++ app/models/outgoing_webhook.rb
@@ -4,14 +4,23 @@ class OutgoingWebhook < Webhook
   belongs_to :processor, optional: true
 
   def notify_transaction(transaction)
-    webhook_payload = WebhookPayload.new(self.system, transaction)
-    body = webhook_payload.get_payload.to_json
+    payload = WebhookPayload.new(self.system, transaction).get_payload
+    post_payload(payload)
+  end
+
+  def notify_recurring(recurring_donor)
+    payload = RecurringWebhookPayload.new(self.system, recurring_donor).get_payload
+    post_payload(payload)    
+  end
+
+  def post_payload(payload)
+    json_payload = payload.to_json
 
     connection = Faraday.new(url: url)
     connection.post do |request|
       request.url url
       request.headers['Content-Type'] = 'application/json'
-      request.body = body
+      request.body = json_payload
     end
   end
 end
