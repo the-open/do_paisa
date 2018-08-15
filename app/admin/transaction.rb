1b15b040cadfd4738da24573eb8090f1b37a9095
Jonathan Ackerley <ackerley.jonathan@gmail.com>
2018-08-15 16:16:58 -0400

    Implement logic for refunds on the active admin transaction view

--- app/admin/transaction.rb
+++ app/admin/transaction.rb
@@ -5,4 +5,12 @@ ActiveAdmin.register Transaction do
   remove_filter :donor, :recurring_donor, :external_id, :source_system, :source_external_id
   filter :donor_email, as: :string
   filter :donor_name, as: :string
+  member_action :refund, method: :put do
+    processor = Processor.find(resource.processor_id)
+    processor.refund(resource.external_id)
+    redirect_to admin_transaction_path, notice: "Transaction has been refunded"
+  end
+  action_item :refund, :only => [:show] , :if => proc { transaction.status != 'refunded' } do
+    link_to "Refund Transaction", refund_admin_transaction_path(transaction), method: :put
+  end
 end
