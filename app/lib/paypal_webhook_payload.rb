2a43e496309d50d637ddca7d4dc078d2a0af0885
Jonathan Ackerley <ackerley.jonathan@gmail.com>
2019-03-20 12:08:36 -0400

    Webhook for paypal en tokens

---
+++ app/lib/paypal_webhook_payload.rb
@@ -0,0 +1,69 @@
+class PaypalWebhookPayload
+    def initialize(system, recurring_donor, processor)
+      @system = system
+      @recurring_donor = recurring_donor
+      @processor = processor
+    end
+  
+    def get_payload
+      case @system
+      when 'identity'
+        identity_payload
+      else
+        default_payload
+      end
+    end
+  
+    private
+  
+    def default_payload
+      {}
+    end
+  
+    def identity_payload
+      payload = {
+        api_token: Rails.application.secrets.identity_api_token,
+        cons_hash: {
+          firstname: @recurring_donor.donor.metadata['first_name'],
+          lastname: @recurring_donor.donor.metadata['last_name'],
+          emails: [
+            { email: (@recurring_donor.donor.metadata['email'] || 'no-email@leadnow.ca') }
+          ],
+          addresses: [
+            {
+              line1: @recurring_donor.donor.metadata['address_line1'],
+              line2: @recurring_donor.donor.metadata['address_line2'],
+              town: @recurring_donor.donor.metadata['address_city'],
+              state: @recurring_donor.donor.metadata['address_state'],
+              postcode: @recurring_donor.donor.metadata['address_zip'],
+              country: @recurring_donor.donor.metadata['address_country']
+            }
+          ]
+        },
+        medium: 'paypal en import',
+        frequency: 'Monthly',
+        source: @recurring_donor.donor.source_external_id,
+        smartdebit_reference: @recurring_donor.donor.metadata['en_id'],
+        started_at: @recurring_donor.created_at,
+        updated_at: @recurring_donor.updated_at,
+        current_amount: '%.2f' % (@recurring_donor.amount / 100.to_f).round(2),
+        ended_at: @recurring_donor.ended_at,
+        external_id: @recurring_donor.donor.metadata['identity_external_id']
+      }
+  
+      if @recurring_donor.donor.metadata['po_guid'].present?
+        payload.merge!({
+          source: @recurring_donor.donor.metadata['po_guid']
+        })
+      end
+  
+      ended_reason = @recurring_donor.last_fail_reason.present? ? @recurring_donor.last_fail_reason : @recurring_donor.cancelled_reason
+      if @recurring_donor.ended_at.present? && ended_reason
+        payload.merge!({
+          ended_reason: ended_reason
+        })
+      end
+  
+      payload
+    end
+  end
