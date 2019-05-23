a45bb40371f2932bfd49ad9f10ec9cf2478b42da
Alex Flint <me@alexf.ca>
2019-05-23 14:27:19 -0400

    delaying emails to avoid deserialization errors

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -23,9 +23,7 @@ class Transaction < ApplicationRecord
   end
 
   def notify_email_approved
-    if !recurring_donor
-      NotificationMailer.with(transaction: self).one_off_approved.deliver_later
-    end
+    NotificationMailer.with(transaction: self).one_off_approved.deliver_later(wait: 1.minute) unless recurring_donor
     post_to_slack
   end
 
@@ -34,9 +32,7 @@ class Transaction < ApplicationRecord
   end
 
   def notify_email_pending
-    if !recurring_donor
-      NotificationMailer.with(transaction: self).one_off_pending.deliver_later
-    end
+    NotificationMailer.with(transaction: self).one_off_pending.deliver_later(wait: 1.minute) unless recurring_donor
   end
 
   def should_send_email_rejected?
@@ -46,9 +42,9 @@ class Transaction < ApplicationRecord
 
   def notify_email_rejected
     if recurring_donor
-      NotificationMailer.with(transaction: self).recurring_fail.deliver_later
+      NotificationMailer.with(transaction: self).recurring_fail.deliver_later(wait: 1.minute)
     else
-      NotificationMailer.with(transaction: self).one_off_pending_rejected.deliver_later
+      NotificationMailer.with(transaction: self).one_off_pending_rejected.deliver_later(wait: 1.minute)
     end
   end
 
