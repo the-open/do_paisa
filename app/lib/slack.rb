b01a604d6c06bfa2cf19d7056c2fe198bfb736f9
Alex Flint <me@alexf.ca>
2019-05-30 10:59:53 -0400

    refactored

--- app/lib/slack.rb
+++ app/lib/slack.rb
@@ -1,7 +1,7 @@
 class Slack
   def post_message(message)
-    if ENV['SLACK_URL']
-      HTTPClient.new.post(ENV['SLACK_URL'], {text: message}.to_json)
-    end
+    return unless ENV['SLACK_URL']
+
+    HTTPClient.new.post(ENV['SLACK_URL'], { text: message }.to_json)
   end
 end
